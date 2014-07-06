a425f6d7e31bf0cc0268d07f630a8bd4ea9db5f8
James Game <james.game.64@gmail.com>
2014-07-06 23:30:48 +0200

    Finished implementation of math functions

--- lib-daynight.js
+++ lib-daynight.js
@@ -9,40 +9,57 @@ function julianToEpoch(julian)
   return ( julian - 2440587.5 ) / 86400.0;
 }
 
+function sunriseCalculus(seconds, latitude, longitude)
+{
+  data = {};
+  
+  //Julian date
+  data.Jdate = epochToJulian(seconds);
+  
+  //Julian cycle since Jan 1st 2000
+  data.nx = data.Jdate - 2451545.0009 - longitude / 360;
+  data.n = data.nx + 0.5;
+  
+  //Approx. solar noon
+  data.Jnoon = 2451545.0009 + longitude / 360 + data.n;
+  
+  //Solar mean anomaly
+  data.M = ( 357.5291 + 0.98560028 * ( data.Jnoon - 2451545)) & 360;
+  
+  //Equation of center
+  data.C = 1.9148 * Math.sin(data.M) + 0.0200 * Math.sin(2*data.M) + 0.0003 * Math.sin(3*data.M);
+  //Ecliptic longitude
+  data.y = ( data.M + 102.9372 + data.C + 180 ) & 360;
+  
+  //Solar transit
+  data.Jtransit = data.Jnoon + 0.0053 * Math.sin(data.M) - 0.0069 * Math.sin(2*data.y);
+  
+  //Sun's declination
+  data.sinDelta = Math.sin(data.y) * Math.sin(23.45);
+  data.delta = Math.asin(data.sinDelta);
+  
+  //Hour angle
+  data.cosWo = ( Math.sin(-0.83) - Math.sin(latitude) * data.sinDelta ) / ( Math.cos(latitude) * Math.cos(data.delta) );
+  data.Wo = Math.acos(data.cosWo);
+  
+  return data;
+}
+
 //Library functions
 function getSunrise_epoch(seconds, latitude, longitude)
 {
-  //Julian date
-  Jdate = epochToJulian(seconds);
-  
-  //Julian cycle since Jan 1st 2000
-  nx = Jdate - 2451545.0009 - longitude / 360;
-  n = nx + 0.5;
-  
-  //Approx. solar noon
-  Jnoon = 2451545.0009 + longitude / 360 + n;
-  
-  //Solar mean anomaly
-  M = ( 357.5291 + 0.98560028 * ( Jnoon - 2451545)) & 360;
-  
-  //Equation of center
-  C = 1.9148 * Math.sin(M) + 0.0200 * Math.sin(2*M) + 0.0003 * Math.sin(3*M);
-  //Ecliptic longitude
-  y = ( M + 102.9372 + C + 180 ) & 360;
-  
-  //Solar transit
-  Jtransit = Jnoon + 0.0053 * Math.sin(M) - 0.0069 * Math.sin(2*y);
-  
-  //Sun's declination
-  sinDelta = Math.sin(y) * Math.sin(23.45);
-  delta = Math.asin(sinDelta);
-  
-  //Hour angle
-  cosWo = ( Math.sin(-0.83) - Math.sin(latitude) * sinDelta ) / ( Math.cos(latitude) * Math.cos(delta) );
-  Wo = Math.acos(cosWo);
+  data = sunriseCalculus(seconds, latitude, longitude);
   
   //Sunrise
+  return 2451545.0009 + (data.Wo + longitude) / 360 + n + 0.0053 * Math.sin(data.M) - 0.0069 * Math.sin(2*data.y);
 }
 
 function getSunset_epoch(seconds, latitude, longitude)
-{}
+{
+  data = sunriseCalculus(seconds, latitude, longitude);
+  
+  //Sunrise
+  Jset = 2451545.0009 + (data.Wo + longitude) / 360 + n + 0.0053 * Math.sin(data.M) - 0.0069 * Math.sin(2*data.y);
+  
+  return data.Jtransit - ( Jset - data.Jtransit);
+}
