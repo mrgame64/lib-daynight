0f4c3b3fbff8ecf42fed750717e6efaaf6f9abef
James Game <james.game.64@gmail.com>
2014-07-06 16:17:51 +0200

    Initial design

---
+++ lib-daynight.js
@@ -0,0 +1,13 @@
+function getSunrise_date(hour, minute, second, day, month, year, longitude)
+
+function getSunrise_millisUTC(ms, longitude)
+{};
+
+function getSunset_millisUTC(ms, longitude)
+{};
+
+function getSunrise_nanosUTC(ns, var longitude)
+{};
+
+function getSunset_nanosUTC(ns, longitude)
+{};
